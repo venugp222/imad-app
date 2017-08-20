4c23f8da3ffeaf3e7cc3d4523ce0d51de9155afa
venugp222 <venugp222@gmail.com>
2017-08-20 13:35:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 app.get('/article-one',function (req,res){
-    res.send("First article");
+     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two',function (req,res){
     res.send("second article");
