eca8e791fb7ae61f6a472c39136e33f7deb97a64
venugp222 <venugp222@gmail.com>
2017-08-20 21:24:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var element = document.getElementById('main-text');
 element.innerHTML='new value';
 
 //move the image
-var img = document.getElementById('img');
+var img = document.getElementById('madi');
 img.onclick = function()
 {
     img.style.marginleft='100px';
