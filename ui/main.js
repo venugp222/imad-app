1d0c880b4391d54e76ecc39ef651b6ad3e039925
venugp222 <venugp222@gmail.com>
2017-08-20 21:22:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,9 @@ console.log('Loaded!');
 //change the text of main - text div
 var element = document.getElementById('main-text');
 element.innerHTML='new value';
+
+//move the image
+var img = document.getElementById('img');
+img.onclick=function(){
+    img.style.marginleft='100px';
+};
