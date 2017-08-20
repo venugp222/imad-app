3ad18cea551322da21bac461adea5d5092c492ca
venugp222 <venugp222@gmail.com>
2017-08-20 13:54:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/article-two',function (req,res){
     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 app.get('/article-three',function (req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-Three.html'));
+    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
